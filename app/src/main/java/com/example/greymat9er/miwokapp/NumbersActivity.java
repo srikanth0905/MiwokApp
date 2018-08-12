5994c8dba508e064d70dee29ae912f52f75b828c
Srikanth <srikanth0905@gmail.com>
2018-08-12 21:02:52 +0530

    changed LinearLayout to ListView in xml file replaced with ArrayAdapter and ListView

--- app/src/main/java/com/example/greymat9er/miwokapp/NumbersActivity.java
+++ app/src/main/java/com/example/greymat9er/miwokapp/NumbersActivity.java
@@ -3,7 +3,9 @@ package com.example.greymat9er.miwokapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -38,12 +40,9 @@ public class NumbersActivity extends AppCompatActivity {
         rootView.addView(wordView);
         */
 
-        for (int i=0; i<englishWords.size(); i++){
-            TextView wordView = new TextView(this);
-            wordView.setText(englishWords.get(i));
-            wordView.setPadding(0,0,0,80);
-            rootView.addView(wordView);
-        }
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, englishWords);
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
 
     }
 
