29a3bbfbf7032cb0e2024238928ec4efd988d3cf
Srikanth <srikanth0905@gmail.com>
2018-08-12 16:21:47 +0530

    removed redundant TextView while finding Views removed commented onClick methods for starting activity

--- app/src/main/java/com/example/greymat9er/miwokapp/MainActivity.java
+++ app/src/main/java/com/example/greymat9er/miwokapp/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Find the view that shows numbers category
-        TextView numbers = (TextView) findViewById(R.id.NumbersView);
+        TextView numbers = findViewById(R.id.NumbersView);
 
         //set a click listener to that View
         numbers.setOnClickListener(new View.OnClickListener() {
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //Find the view that shows Colors category
-        TextView colors = (TextView) findViewById(R.id.ColorsView);
+        TextView colors = findViewById(R.id.ColorsView);
 
         //set a click listener to that View
         colors.setOnClickListener(new View.OnClickListener() {
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //Find the view that shows Phrases category
-        TextView phrases = (TextView) findViewById(R.id.PhrasesView);
+        TextView phrases = findViewById(R.id.PhrasesView);
 
         //set a click listener to that View
         phrases.setOnClickListener(new View.OnClickListener() {
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //Find the view that shows Family category
-        TextView family = (TextView) findViewById(R.id.FamilyView);
+        TextView family = findViewById(R.id.FamilyView);
 
         //set a click listener to that View
         family.setOnClickListener(new View.OnClickListener() {
@@ -66,27 +66,5 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-    /*
-    public void numbersList(View view){
-        Intent i = new Intent(this, NumbersActivity.class);
-        startActivity(i);
-    }
-
-
-    public void colorsList(View view){
-        Intent i = new Intent(this, Colors.class);
-        startActivity(i);
-    }
-
-    public void phrasesList(View view){
-        Intent i = new Intent(this, Phrases.class);
-        startActivity(i);
-    }
-
-    public void familyList(View view){
-        Intent i = new Intent(this, FamilyMembers.class);
-        startActivity(i);
-    }
-    */
 
 }
